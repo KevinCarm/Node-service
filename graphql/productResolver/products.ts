38347e8d54d3c2f55dfe33a6b238887dc555a4f8
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-12-14 22:07:55 -0600

    Add functionality to fetch all products

---
+++ graphql/productResolver/products.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import MongooseProduct from "../../models/Product";
+import { Product } from "../graphqlSchemasTypes";
+
+class ErrorException extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+const products = async (
+    args: any,
+    req: express.Request
+): Promise<Product[]> => {
+    const userId = String(req.get("userId")?.toString());
+    if (!userId || userId === "false") {
+        const error: ErrorException = new ErrorException("No authenticated");
+        throw error;
+    }
+
+    const products = await MongooseProduct.find().populate("creator");
+    const finalList: Product[] = products.map(p => {
+        const product: Product = {
+            _id: String(p._id),
+            name: p.name,
+            price: p.price,
+            description: p.description,
+            quantity: p.quantity,
+            creator: p.creator,
+        };
+        return product;
+    });
+
+    return finalList;
+};
+
+export default products;
--- graphql/resolver.ts
+++ graphql/resolver.ts
@@ -1,9 +1,11 @@
+import products from "./productResolver/products";
 import registerUser from "./userResolver/create";
 import login from "./userResolver/login";
 
 const query = {
     registerUser: registerUser,
     login: login,
+    products: products,
 };
 
 export default query;
