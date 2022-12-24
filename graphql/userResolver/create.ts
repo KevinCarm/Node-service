0851ca70766293a31a066991199bf1ed957ab166
Kevin Carmona Serrano <kevincarm016@hotmail.com>
2022-12-24 17:14:17 -0600

    Bugfix, throw an exception if user input is not valid

--- graphql/userResolver/create.ts
+++ graphql/userResolver/create.ts
@@ -5,35 +5,41 @@ import validator from "validator";
 import bcrypt from "bcryptjs";
 import express from "express";
 
-class ErrorException extends Error {
-    constructor(message: string) {
-        super(message);
-    }
-}
-
 const registerUser = async (args: any, req: express.Request): Promise<User> => {
     const isAuth = req.headers["isAuth"];
     if (isAuth === "false") {
         const error: Error = new Error(
-            JSON.stringify({ message: "No authenticated", code: 401 })
+            JSON.stringify({
+                message: "No authenticated",
+                code: 401,
+            })
         );
         throw error;
     }
     if (!validator.isEmail(args.userInputData.email)) {
-        const error: ErrorException = new ErrorException(
-            "Invalid email, enter a valid email"
+        const error: Error = new Error(
+            JSON.stringify({
+                message: "Invalid email, enter a valid email",
+                code: 400,
+            })
         );
         throw error;
     }
     if (validator.isEmpty(args.userInputData.name)) {
-        const error: ErrorException = new ErrorException(
-            "Invalid name, it must not be empty"
+        const error: Error = new Error(
+            JSON.stringify({
+                message: "Invalid name, enter a valid name",
+                code: 400,
+            })
         );
         throw error;
     }
     if (!validator.isLength(args.userInputData.password, { min: 5 })) {
-        const error: ErrorException = new ErrorException(
-            "Invalid password, it must be att least 5 characters"
+        const error: Error = new Error(
+            JSON.stringify({
+                message: "Invalid password, it must be att least 5 characters",
+                code: 400,
+            })
         );
         throw error;
     }
@@ -42,13 +48,16 @@ const registerUser = async (args: any, req: express.Request): Promise<User> => {
         email: args.userInputData.email,
     });
     if (existingUser) {
-        const error: ErrorException = new ErrorException(
-            "User email already exists"
+        const error: Error = new Error(
+            JSON.stringify({
+                message: "Email already exists",
+                code: 400,
+            })
         );
         throw error;
     }
     const roleUser = await MongooseRole.findOne({ name: "ROLE_USER" });
-   
+
     const hashedPassword = await bcrypt.hash(args.userInputData.password, 12);
     const user = new MongoUser({
         name: args.userInputData.name,
